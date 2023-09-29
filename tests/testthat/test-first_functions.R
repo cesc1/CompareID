68755f15c2485635a7b3e54f7325e7bba60d7856
45884349q <francescaguirre@gencat.cat>
2023-09-29 13:39:47 +0200

    Fix test data import
    Can be fixed using testthat::test_path(), and saving the data files inside tests/testthat, more especificaly inside the folder test_data

--- tests/testthat/test-first_functions.R
+++ tests/testthat/test-first_functions.R
@@ -1,5 +1,14 @@
-test_that("load_data in my data", {
+test_that("load_data: used with my data", {
+  data_load <- load_data(
+    test_path("test_data", "Dades prova.xlsx")
+  )
 
-  expect_equal(load_data(), 4)
+  data_check <- data.frame(
+    municipi = c("Abrera", "Aguilar", "Alélla", "Alpans",
+                 "Ametlla Vallès", "Arenys Mar", "Espunyola, l'",
+                 "Terrassa", "Barcelona"), # Import delete spaces in start-end
+    info = c(1:9)
+  )
 
+  expect_equal(data_load, data_check)
 })
