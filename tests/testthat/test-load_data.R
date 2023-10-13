367caa0b4b9ca5573695725c9eb3cae562546993
45884349q <francescaguirre@gencat.cat>
2023-10-13 10:19:57 +0200

    [dev] Fix data load test
    Added the data from the vignette to tests, to test the load_data() function.

--- tests/testthat/test-load_data.R
+++ tests/testthat/test-load_data.R
@@ -2,15 +2,14 @@
 # Test load_data: Càrrega de dades
 
 test_that("load_data: used with my data", {
-  path = test_path("test_data", "Dades prova.xlsx")
+  path = test_path("data test", "Dades vignette.xlsx")
 
-  data_check <- data.frame(
+  data_check <- dplyr::tibble(
     municipi = c("Abrera", "Aguilar", "Alélla", "Alpans",
                  "Ametlla Vallès", "Arenys Mar", "Espunyola, l'",
                  "Terrassa", "Barcelona"), # Import delete spaces in start-end
     info = c(1:9)
   )
 
-  expect_equal(load_data(path), data_check)
   expect_equal(load_data(path, which = "Hoja1"), data_check)
 })
